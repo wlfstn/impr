8ebe75ef45525562e0e2a3b6903d62581119a275
CorpoWolf <gmangavin1@gmail.com>
2025-07-31 14:13:22 -0700

    some rust interactivity

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-	println!("Hello from impr");
+	let args: Vec<String> = env::args().collect();
+
+	let name: &str = if args.len() > 1 {
+		&args[1]
+	} else {
+		"World"
+	};
+
+	println!("Hello {} from impr", name);
 }
